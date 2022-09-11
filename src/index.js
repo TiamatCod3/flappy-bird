858cf814308b19acbfbc47e359dea0eb2e0363af
TiamatCod3 <ti_matos@id.uff.br>
2022-09-10 21:36:49 -0300

    Scene positioning

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import Phaser from 'phaser';
 
-new Phaser.Game();
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics:{
+    default: 'arcade'
+  },
+  scene: {
+    preload,
+    create
+  }
+}
+
+//Loading assets, such images, music, animations ....
+function preload(){
+  //this contxt - scene
+  // contains funciton and propoesties tha can use
+  this.load.image('sky', 'assets/sky.png');
+  
+}
+
+//Initializae instances of application
+function create(){
+  //x e y da imagem ficam centralizados no canto superior esquerdo
+  // this.add.image(config.width / 2 , config.height / 2, 'sky').setOrigin(0,0);
+  // this.add.image(0, 0, 'sky').setOrigin(0,0);
+  // this.add.image(0, 0, 'sky').setOrigin(0.9,0);
+  this.add.image(config.width / 2 , config.height / 2, 'sky').setOrigin(1,0);
+
+}
+
+new Phaser.Game(config);
