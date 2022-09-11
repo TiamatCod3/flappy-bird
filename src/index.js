3dafdf3490d19354bb230c75163140d855284154
TiamatCod3 <ti_matos@id.uff.br>
2022-09-10 23:47:37 -0300

    16. Restarting the game

--- src/index.js
+++ src/index.js
@@ -30,7 +30,8 @@ const WIDTH = config.width;
 const VELOCITY = 250;
 const flapVelocity = 250;
 let bird = null;
-let totalDelta = null;
+let initialBirdPosition = {x:config.width * 0.1, y:config.height/2};
+
 
 //Initializae instances of application
 function create(){
@@ -40,7 +41,7 @@ function create(){
   // this.add.image(0, 0, 'sky').setOrigin(0.9,0);
   // this.add.image(config.width / 2 , config.height / 2, 'sky').setOrigin(1,0);
   // Phisics addition
-  bird = this.physics.add.sprite(config.width * 0.1, config.height/2, 'bird').setOrigin(0);
+  bird = this.physics.add.sprite(initialBirdPosition.x,initialBirdPosition.y, 'bird').setOrigin(0);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown_SPACE', flap);
@@ -52,14 +53,15 @@ function create(){
 //delta is the time between frames
 //By default it is setted to 60fps and delta of 16.6ms
 function update(time, delta){
-
-  if(bird.x >= WIDTH - bird.width){
-    bird.body.velocity.x = -VELOCITY;
-  }else if(bird.x < 0){
-    bird.body.velocity.x = VELOCITY;
+  if(bird.y > config.height || bird.y < -bird.height){
+    restartBirdPosition();
   }
+}
 
-
+function restartBirdPosition(){
+  bird.y = initialBirdPosition.y;
+  bird.x = initialBirdPosition.x;
+  bird.body.velocity.y = 0;
 }
 
 function flap(){
